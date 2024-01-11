a58a11251f5c5d43d0f391671d0f74b8ac3b65f5
ceeb <ceeb9999@gmail.com>
2024-01-11 18:37:06 +0800

    pixelbuffer class

--- main.cpp
+++ main.cpp
@@ -8,6 +8,7 @@ class Color {
         int red;
         int green;
         int blue;
+        Color() {}
         Color(int input_red, int input_green, int input_blue) {
             this->red = input_red;
             this->green = input_green;
@@ -15,35 +16,58 @@ class Color {
         }
 };
 
+class PixelBuffer {
+    private:
+        vector<vector<Color>> pixels;
+
+    public:
+        int SIZE_X;
+        int SIZE_Y;
+        PixelBuffer(int input_size_x, int input_size_y) {
+            this->SIZE_X = input_size_x;
+            this->SIZE_Y = input_size_y;
+            this->pixels = vector<vector<Color>>(input_size_y, vector<Color>(input_size_x, Color(0, 0, 0)));
+        }
+
+        Color get_pixel(int x, int y) {
+            return this->pixels[y][x];
+        }
+
+        void set_pixel(int x, int y, Color new_color) {
+            this->pixels[y][x] = new_color;
+        }
+};
+
 int main() {
     Color c1(255, 0, 0);
     Color c2(0, 255, 0);
     Color c3(0, 0, 255);
-    Color c4(0, 0, 0);
+    Color c4(255, 255, 0);
 
-    vector<vector<Color>> pixel_buffer(4, vector<Color>(4, Color(0,0,0)));
-    pixel_buffer[0][0] = Color(255, 0, 0);
-    pixel_buffer[0][1] = Color(0, 255, 0);
-    pixel_buffer[0][2] = Color(0, 0, 255);
-    pixel_buffer[0][3] = Color(255, 255, 0);
+    PixelBuffer pixel_buffer(4, 4);
+    pixel_buffer.set_pixel(0, 0, c1);
+    pixel_buffer.set_pixel(0, 1, c2);
+    pixel_buffer.set_pixel(0, 2, c3);
+    pixel_buffer.set_pixel(0, 3, c4);
 
     string textframe = "";
 
-    for (int y = 0; y < pixel_buffer.size(); y+=2) {
-        for (int x = 0; x < pixel_buffer[0].size(); x++) {
-            //cout << x << ", " << y << ": " << pixel_buffer[y][x].red << " " << pixel_buffer[y][x].green << " " << pixel_buffer[y][x].blue << "\n";
+    for (int y = 0; y < pixel_buffer.SIZE_Y; y+=2) {
+        for (int x = 0; x < pixel_buffer.SIZE_X; x++) {
             
             //background color
             textframe += "\033[48;2;";
-            textframe += to_string(pixel_buffer[y][x].red) + ";";
-            textframe += to_string(pixel_buffer[y][x].green) + ";";
-            textframe += to_string(pixel_buffer[y][x].blue) + "m";
+            Color upper_pixel = pixel_buffer.get_pixel(x, y);
+            Color lower_pixel = pixel_buffer.get_pixel(x, y+1);
+            textframe += to_string(upper_pixel.red) + ";";
+            textframe += to_string(upper_pixel.green) + ";";
+            textframe += to_string(upper_pixel.blue) + "m";
             
             //foreground color
             textframe += "\033[38;2;";
-            textframe += to_string(pixel_buffer[y+1][x].red) + ";";
-            textframe += to_string(pixel_buffer[y+1][x].green) + ";";
-            textframe += to_string(pixel_buffer[y+1][x].blue) + "m";
+            textframe += to_string(lower_pixel.red) + ";";
+            textframe += to_string(lower_pixel.green) + ";";
+            textframe += to_string(lower_pixel.blue) + "m";
             textframe += "▄";
         }
         textframe += "\n";
